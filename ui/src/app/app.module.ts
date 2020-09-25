01b3e5ff06175493f9d9184c30f5c3842bbd8225
Chandan Y S <chandanys18@gmail.com>
2020-09-25 17:59:37 +0530

    project finalized

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GoogleMapsModule } from '@angular/google-maps';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
